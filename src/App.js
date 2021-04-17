ad39c7561446caf7f8168466776a648752894138
Denis Pankov <idenispankov@gmail.com>
2021-04-17 14:36:56 -0700

    markup and styling ready

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+const TodoItem = () => {
+  return (
+    <ul className='list'>
+      <li className='list__item'>
+        Some todo
+        <span className='list__item-button'>X</span>
+      </li>
+    </ul>
+  );
+};
+
+const Form = () => {
+  return (
+    <form className='form' autoComplete='off'>
+      <input type='text' className='form__input' placeholder='Add Todo...' />
+      <button className='form__button'>Add</button>
+    </form>
+  );
+};
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='page__container'>
+      <Form />
+      <TodoItem />
+    </div>
+  );
 }
 
 export default App;
